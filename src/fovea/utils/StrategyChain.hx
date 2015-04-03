1702403796f888cee9d02cb9cf8db432279d80e4
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-04-03 23:46:12 +0300

    Give public access to the strategies array

--- src/fovea/utils/StrategyChain.hx
+++ src/fovea/utils/StrategyChain.hx
@@ -5,7 +5,8 @@ import openfl.utils.Object;
 @:expose
 class StrategyChain extends Strategy
 {
-    private var _strategies:Array<Strategy>;
+    public var strategies:Array<Strategy>;
+
     public function new(strategies:Array<Strategy>) {
         super(function(json:Object):Bool { // canExecute
             // if one of the strategies canExecute
@@ -25,14 +26,14 @@ class StrategyChain extends Strategy
             return null;
         });
 
-        _strategies = strategies;
+        this.strategies = strategies;
     }
 
     public function executeAll(json:Object):Object {
         var ret:Object = [];
-        for (i in 0 ... _strategies.length) {
-            if (_strategies[i].canExecute(json))
-                ret.push(_strategies[i].execute(json));
+        for (i in 0 ... strategies.length) {
+            if (strategies[i].canExecute(json))
+                ret.push(strategies[i].execute(json));
             else
                 ret.push(null);
         }
