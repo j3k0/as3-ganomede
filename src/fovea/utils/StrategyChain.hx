674555c9c7728ffda49b8cfc848155e7c9bdcc75
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-04-03 18:36:27 +0300

    Add executeAll in StrategyChain

--- src/fovea/utils/StrategyChain.hx
+++ src/fovea/utils/StrategyChain.hx
@@ -5,8 +5,8 @@ import openfl.utils.Object;
 @:expose
 class StrategyChain extends Strategy
 {
+    private var _strategies:Array<Strategy>;
     public function new(strategies:Array<Strategy>) {
-
         super(function(json:Object):Bool { // canExecute
             // if one of the strategies canExecute
             for (i in 0 ... strategies.length) {
@@ -24,6 +24,19 @@ class StrategyChain extends Strategy
             }
             return null;
         });
+
+        _strategies = strategies;
+    }
+
+    public function executeAll(json:Object):Object {
+        var ret:Object = [];
+        for (i in 0 ... _strategies.length) {
+            if (_strategies[i].canExecute(json))
+                ret.push(_strategies[i].execute(json));
+            else
+                ret.push(null);
+        }
+        return ret;
     }
 }
 
