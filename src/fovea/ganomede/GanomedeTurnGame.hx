b85368a219ae935e5dc69d1adb9d71175b0c83b9
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-05-17 19:23:31 +0300

    GanomedeTurnGame dispatches update events

--- src/fovea/ganomede/GanomedeTurnGame.hx
+++ src/fovea/ganomede/GanomedeTurnGame.hx
@@ -25,14 +25,27 @@ class GanomedeTurnGame extends Model {
     }
 
     public override function fromJSON(obj:Object):Void {
-        if (obj.id) id = obj.id;
-        if (obj.type) type = obj.type;
-        if (obj.players) players = obj.players;
-        if (obj.turn) turn = obj.turn;
-        if (obj.status) status = obj.status;
-        if (obj.gameData) gameData = obj.gameData;
-        if (obj.gameConfig) gameConfig = obj.gameConfig;
-        if (obj.url) url = obj.url;
+        if (obj.id && id != obj.id) { id = obj.id; dispatchUpdate(); }
+        if (obj.type && type != obj.type) { type = obj.type; dispatchUpdate(); }
+        if (obj.players && players != obj.players) { players = obj.players; dispatchUpdate(); }
+        if (obj.turn && turn != obj.turn) { turn = obj.turn; dispatchUpdate(); }
+        if (obj.status && status != obj.status) { status = obj.status; dispatchUpdate(); }
+        if (obj.gameData && gameData != obj.gameData) { gameData = obj.gameData; dispatchUpdate(); }
+        if (obj.gameConfig && gameConfig != obj.gameConfig) { gameConfig = obj.gameConfig; dispatchUpdate(); }
+        if (obj.url && url != obj.url) { url = obj.url; dispatchUpdate(); }
+    }
+
+    private var listeners = new Array<Void->Void>();
+    public function addListener(fn:Void->Void):Void {
+        listeners.push(fn);
+    }
+    public function removeListener(fn:Void->Void):Void {
+        listeners.remove(fn);
+    }
+    function dispatchUpdate():Void {
+        for (i in 0 ... listeners.length) {
+            listeners[i]();
+        }
     }
 
     public override function toJSON():Object {
