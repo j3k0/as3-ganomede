648a0c03cac3b8038e2e45446af37a7436852dbe
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-12-09 23:13:44 +0100

    Add 'chatEvent' field to GanomedeTurnData
    Allows to attach system events to a move that has been made in the game.
    The chat event will be add to the chat room linked to the game
    being played, as a system message.

--- src/fovea/ganomede/GanomedeTurnMove.hx
+++ src/fovea/ganomede/GanomedeTurnMove.hx
@@ -7,6 +7,7 @@ import fovea.utils.Model;
 class GanomedeTurnMove extends Model {
     public var moveData:Object;
     public var moveResult:Object;
+    public var chatEvent:String;
 
     public function new(obj:Object = null) {
         super(obj);
@@ -16,13 +17,15 @@ class GanomedeTurnMove extends Model {
         if (obj.id) id = obj.id;
         if (obj.moveData) moveData = obj.moveData;
         if (obj.moveResult) moveResult = obj.moveResult;
+        if (obj.chatEvent) chatEvent = obj.chatEvent;
     }
 
     public override function toJSON():Object {
         return {
             id:id,
             moveData:moveData,
-            moveResult:moveResult
+            moveResult:moveResult,
+            chatEvent:chatEvent
         };
     }
 }
