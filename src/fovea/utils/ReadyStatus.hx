4bb8f5093b172ce06b6fddaa384f2610f55aea25
Jean-Christophe Hoelt <hoelt@fovea.cc>
2016-04-01 12:43:53 +0300

    Add ReadyStatus.setError
    Allow to dispatch initialization failure to ReadyStatus listeners.

--- src/fovea/utils/ReadyStatus.hx
+++ src/fovea/utils/ReadyStatus.hx
@@ -2,33 +2,48 @@ package fovea.utils;
 
 import fovea.async.*;
 import fovea.ganomede.helpers.*;
+import openfl.errors.Error;
 
 @:expose
 class ReadyStatus
 {
     public var isReady(default,null):Bool = false;
+    public var error(default,null):Error = null;
     private var readyCallbacks = new Array<Deferred>();
 
     public function reset():Void {
         isReady = false;
+        error = null;
         readyCallbacks = new Array<Deferred>();
     }
 
     public function ready():Promise {
         var deferred:Deferred = new Deferred();
-        if (this.isReady)
+        if (isReady)
             deferred.resolve(null);
+        else if (error != null)
+            deferred.reject(error);
         else
             readyCallbacks.push(deferred);
         return deferred;
     }
  
     public function setReady():Void {
-        this.isReady = true;
+        isReady = true;
+        error = null;
         var a = readyCallbacks;
         readyCallbacks = new Array<Deferred>();
         for (i in 0...a.length)
             a[i].resolve(null);
+    }
+
+    public function setError(err:Error):Void {
+        error = err;
+        isReady = false;
+        var a = readyCallbacks;
+        readyCallbacks = new Array<Deferred>();
+        for (i in 0...a.length)
+            a[i].reject(err);
     }
 }
 // vim: ts=4:sw=4:et:
